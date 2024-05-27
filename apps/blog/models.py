1ed9dce66163e202ab7bdb7c87cb18c0ef438777
popoffww <popoffww@yandex.ru>
2024-05-27 19:53:32 +0600

    Первая модель

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     """
     Модель постов для нашего блога
--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -22,6 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
