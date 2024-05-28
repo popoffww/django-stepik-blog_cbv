65f5844f035938624c8ca9ddc858b0547f12036a
popoffww <popoffww@yandex.ru>
2024-05-28 19:56:32 +0600

    Древовидная модель категорий

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post
+from mptt.admin import DraggableMPTTAdmin
+from django_mptt_admin.admin import DjangoMpttAdmin
+
+
+@admin.register(Category)
+# class CategoryAdmin(DraggableMPTTAdmin):
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
 
 
 admin.site.register(Post)
--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Installed
     "apps.blog.apps.BlogConfig",
+    "mptt",
+    "django_mptt_admin",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +126,5 @@ MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
